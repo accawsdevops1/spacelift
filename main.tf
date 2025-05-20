74ad11683db327cf98f9d9305bb1502e41379f46
accawsdevops <acc.aws.devops@gmail.com>
2025-05-20 16:10:22 +0530

    updating the terratest module

--- main.tf
+++ main.tf
@@ -1,13 +1,53 @@
-terraform {
-  required_version = ">= 0.12.26"
-}
-
 provider "aws" {
-  region = "us-east-2"
+  region = "us-east-1"
 }
 
-# resource "aws_iam_user" "test_user" {
-#   name = "test-user"
-# }
+# Create IAM Groups
+resource "aws_iam_group" "group1" {
+  name = "group1"
+}
 
-#demo line
+resource "aws_iam_group" "group2" {
+  name = "group2"
+}
+
+# Create IAM Users
+resource "aws_iam_user" "user1" {
+  name = "user1"
+}
+
+resource "aws_iam_user" "user2" {
+  name = "user2"
+}
+
+# Add users to groups
+resource "aws_iam_user_group_membership" "user1_membership" {
+  user = aws_iam_user.user1.name
+  groups = [
+    aws_iam_group.group1.name
+  ]
+}
+
+resource "aws_iam_user_group_membership" "user2_membership" {
+  user = aws_iam_user.user2.name
+  groups = [
+    aws_iam_group.group2.name
+  ]
+}
+
+# Outputs for Terratest to verify
+output "iam_user1_name" {
+  value = aws_iam_user.user1.name
+}
+
+output "iam_user2_name" {
+  value = aws_iam_user.user2.name
+}
+
+output "iam_group1_name" {
+  value = aws_iam_group.group1.name
+}
+
+output "iam_group2_name" {
+  value = aws_iam_group.group2.name
+}
